e7fd3572297ac2ad4a5d7778ca6b92ea5a1feddb
pate0718 <pate0718@algonquinlive.com>
2020-11-17 16:21:22 -0500

    learning node

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+/*
+const express = require('express');
+const app = express();
+let port = 3000;
+app.get('/',(req,res)=>{
+    res.send("hello world");
+});
+*/
+
+
+hello();
+function hello(){ console.log("hello") };
+let x=10;
+
+console.log(x);
